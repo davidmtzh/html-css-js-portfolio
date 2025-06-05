9760b5742b87d426ab69414ceec6d4ca0ab82535
cmartinez1542 <cmartinez1542@sdsu.edu>
2025-06-04 22:08:36 -0700

    -Updated profile pictures. -Color boxes for projects -Project boxes expand downward

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+
+
+
+
 function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+    const collapseEl = document.getElementById('details1');
+    const cardEl = collapseEl.closest('.custom-expand-card');
+
+    collapseEl.addEventListener('show.bs.collapse', () => {
+        cardEl.classList.add('expanded');
+    });
+
+    collapseEl.addEventListener('hide.bs.collapse', () => {
+        cardEl.classList.remove('expanded');
+    });

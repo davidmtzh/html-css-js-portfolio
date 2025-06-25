f74adc14406e341f26e84afeee110331c4546215
cmartinez1542 <cmartinez1542@sdsu.edu>
2025-06-25 16:31:46 -0700

    added project 1 hover animation

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 
-
-
-
 function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
-    const collapseEl = document.getElementById('details1');
-    const cardEl = collapseEl.closest('.custom-expand-card');
 
-    collapseEl.addEventListener('show.bs.collapse', () => {
-        cardEl.classList.add('expanded');
-    });
 
-    collapseEl.addEventListener('hide.bs.collapse', () => {
-        cardEl.classList.remove('expanded');
-    });
+    const robotImg = document.getElementById('project-img');
+    if (robotImg) {
+      robotImg.addEventListener('mouseenter', () => {
+        robotImg.src = "./assets/robo.gif";
+      });
+  
+      robotImg.addEventListener('mouseleave', () => {
+        robotImg.src = "./assets/project1.png";
+      });
+    }
+  
+    
